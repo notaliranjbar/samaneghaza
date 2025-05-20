9d6ae2042dd7479c3354f900c4b582fa6fdc6c20
Ali <ali123ranjbar456@gmail.com>
2025-05-20 11:23:40 +0330

    creating proffessors and foods and ordered foods table

--- create_tables.sql
+++ create_tables.sql
@@ -1,8 +1,28 @@
 create table students(
-    name nvarchar(50) ,
-    national_code int,
-    id int ,
+    name nvarchar(50) not null,
+    national_code int not null,
+    id int primary key auto_increment,
     major nvarchar(50)
 
 
-)
+);
+create table proffessors(
+    name nvarchar(50) not null,
+    national_code int not null ,
+    id int primary key auto_increment,
+    department nvarchar(50) not null,
+    grade nvarchar(50) not null
+);
+create table foods(
+    name nvarchar(50) not null,
+    price int not null,
+    date Date not null
+);
+create table ordered_foods(
+    food_name nvarchar(50) not null,
+    owner_id int not null,
+    foreign key (food_name) references foods(name),
+    foreign key (owner_id) references students(id),
+    foreign key (owner_id) references proffessors(id),
+
+);
