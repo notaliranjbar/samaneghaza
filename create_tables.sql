fd901e5824de949c09f74a76a572f6b222a97779
Ali <ali123ranjbar456@gmail.com>
2025-05-20 11:51:59 +0330

    debugging

--- create_tables.sql
+++ create_tables.sql
@@ -1,28 +1,29 @@
+use tamrin1;
 create table students(
     name nvarchar(50) not null,
     national_code int not null,
-    id int primary key auto_increment,
+    student_id int primary key auto_increment,
     major nvarchar(50)
 
 
 );
-create table proffessors(
+create table professors(
     name nvarchar(50) not null,
     national_code int not null ,
-    id int primary key auto_increment,
+    professor_id int primary key auto_increment,
     department nvarchar(50) not null,
     grade nvarchar(50) not null
 );
 create table foods(
     name nvarchar(50) not null,
     price int not null,
-    date Date not null
+    id int primary key
 );
 create table ordered_foods(
-    food_name nvarchar(50) not null,
+    food_id int not null,
     owner_id int not null,
-    foreign key (food_name) references foods(name),
-    foreign key (owner_id) references students(id),
-    foreign key (owner_id) references proffessors(id),
+    foreign key (food_id) references foods(id),
+    foreign key (owner_id) references students(student_id),
+    foreign key (owner_id) references professors(professor_id)
 
 );
